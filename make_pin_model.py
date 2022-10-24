cc8cc8750233a2a6d97e58da0fac65c43b64b7cc
Kalin Kiesling <kkiesling@anl.gov>
2022-10-24 15:12:16 -0500

    script for building BEAVRS pin model

---
+++ make_pin_model.py
@@ -0,0 +1,74 @@
+from math import pi
+import openmc
+
+###############################################################################
+#                              Define materials
+###############################################################################
+
+# Instantiate some Materials and register the appropriate Nuclides
+uo2 = openmc.Material(material_id=1, name='UO2 fuel at 2.4% wt enrichment')
+uo2.set_density('g/cm3', 10.29769)
+uo2.add_element('U', 1., enrichment=2.4)
+uo2.add_element('O', 2.)
+
+helium = openmc.Material(material_id=2, name='Helium for gap')
+helium.set_density('g/cm3', 0.001598)
+helium.add_element('He', 2.4044e-4)
+
+zircaloy = openmc.Material(material_id=3, name='Zircaloy 4')
+zircaloy.set_density('g/cm3', 6.55)
+zircaloy.add_element('Sn', 0.014, 'wo')
+zircaloy.add_element('Fe', 0.00165, 'wo')
+zircaloy.add_element('Cr', 0.001, 'wo')
+zircaloy.add_element('Zr', 0.98335, 'wo')
+
+borated_water = openmc.Material(material_id=4, name='Borated water')
+borated_water.set_density('g/cm3', 0.740582)
+borated_water.add_element('B', 4.0e-5)
+borated_water.add_element('H', 5.0e-2)
+borated_water.add_element('O', 2.4e-2)
+borated_water.add_s_alpha_beta('c_H_in_H2O')
+
+###############################################################################
+#                             Create geometry
+###############################################################################
+
+# Create surfaces
+fuel_or = openmc.ZCylinder(r=0.39218, name='Fuel OR')
+clad_ir = openmc.ZCylinder(r=0.40005, name='Clad IR')
+clad_or = openmc.ZCylinder(r=0.45720, name='Clad OR')
+pitch = 1.25984
+box = openmc.model.rectangular_prism(pitch, pitch, boundary_type='reflective')
+
+# Create cells
+fuel = openmc.Cell(fill=uo2, region=-fuel_or)
+gap = openmc.Cell(fill=helium, region=+fuel_or & -clad_ir)
+clad = openmc.Cell(fill=zircaloy, region=+clad_ir & -clad_or)
+water = openmc.Cell(fill=borated_water, region=+clad_or & box)
+
+# Create a geometry
+geometry = openmc.Geometry([fuel, gap, clad, water])
+
+###############################################################################
+#                     Set volumes of depletable materials
+###############################################################################
+
+# Since this is a 2D model, use area instead of volume
+uo2.volume = pi * fuel_or.r**2
+
+###############################################################################
+#                     Transport calculation settings
+###############################################################################
+
+# Instantiate a Settings object, set all runtime parameters, and export to XML
+settings = openmc.Settings()
+settings.batches = 50
+settings.inactive = 10
+settings.particles = 1000
+
+# Create an initial uniform spatial source distribution over fissionable zones
+settings.source = openmc.source.Source(space=openmc.stats.Point())
+
+# write out xml files
+model = openmc.model.Model(geometry=geometry, settings=settings)
+model.export_to_xml()
