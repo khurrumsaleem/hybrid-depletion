6cca63d1d3267edc291ffd79854eb83ec859f4aa
Kalin Kiesling <krkiesling@gmail.com>
2022-11-14 16:14:46 -0600

    add volume info

--- model/make_beavrs_assembly.py
+++ model/make_beavrs_assembly.py
@@ -49,11 +49,33 @@ settings.batches = 80
 settings.inactive = 20
 settings.particles = int(5e5)
 
+# set volume calc
+mat_doms = list(sub_geometry.get_all_materials().values())
+bb = sub_geometry.bounding_box
+mat_calc = openmc.VolumeCalculation(domains=mat_doms,
+                                    samples=100000000,
+                                    lower_left=bb[0],
+                                    upper_right=bb[1])
+settings.volume_calculations = [mat_calc]
+
 # Use a bounding box to define the starting source distribution
 lower_left = [-17*c.pinPitch/2, -17*c.pinPitch/2, c.fuel_ActiveFuel_bot]
 upper_right = [+17*c.pinPitch/2, +17*c.pinPitch/2, c.fuel_ActiveFuel_top]
 settings.source = openmc.source.Source(
     openmc.stats.Box(lower_left, upper_right, only_fissionable=True))
 
+settings.run_mode = 'volume'
+
 # Export the settings to a "settings.xml" file
 settings.export_to_xml()
+
+# run volume calc
+openmc.run()
+
+# load volume info
+vol_info = openmc.VolumeCalculation.from_hdf5('volume_1.h5')
+for mat in materials:
+    mat.add_volume_information(vol_info)
+
+# re-export with volume information
+materials.export_to_xml()
