80c3a688a73bac32fec8b9aa472d29b8572595dd
Kalin Kiesling <kkiesling@anl.gov>
2022-11-16 10:47:21 -0600

    don't reduce chain file if already exists

--- deplete/run_depletion.py
+++ deplete/run_depletion.py
@@ -4,6 +4,7 @@ import numpy as np
 import shutil
 import argparse
 import pathlib
+import os
 
 openmc.deplete.pool.NUM_PROCESSES = 20
 openmc.deplete.pool.USE_MULTIPROCESSING = True
@@ -25,8 +26,8 @@ def parse_args():
                         help='Run all types of depletion calcs')
     parser.add_argument('-m', '--model', default='.', type=pathlib.Path,
                         help='Path to model .xml files and chain file to load. Default cwd.')
-    parser.add_argument('-c', '--chain', default='.', type=pathlib.Path,
-                        help='Path to chain .xml file and chain file to load. Default cwd.')
+    parser.add_argument('-c', '--chain', type=pathlib.Path,
+                        help='Path to chain .xml file and chain file to load.')
     args = parser.parse_args()
 
     return args
@@ -64,17 +65,21 @@ if __name__ == "__main__":
     ###############################################################################
     #                  Reduce Chain File
     ###############################################################################
-    chain_path = str(args.chain)
-    chain_file = chain_path + '/chain_endfb71_pwr.xml'
-    chain_full = openmc.deplete.Chain.from_xml(chain_file)
-    stable = [
-        nuc.name
-        for nuc in chain_full.nuclides
-        if nuc.half_life is None or nuc.half_life > 1e15
-    ]
-    chain_reduced = chain_full.reduce(stable)
-    chain_reduced.export_to_xml(chain_path + '/chain_reduced_pwr.xml')
-    chain_file = chain_path + '/chain_reduced_pwr.xml'
+    chain_file = str(args.chain)
+    chain_red_path = chain_file[:-4] + '_reduced.xml'
+    #chain_file = chain_path + '/chain_endfb71_pwr.xml'
+
+    # don't reduce if already reduced
+    if not os.path.exists(chain_red_path):
+        chain_full = openmc.deplete.Chain.from_xml(chain_file)
+        stable = [
+            nuc.name
+            for nuc in chain_full.nuclides
+            if nuc.half_life is None or nuc.half_life > 1e15
+        ]
+        chain_reduced = chain_full.reduce(stable)
+        chain_reduced.export_to_xml(chain_red_path)
+        chain_file = chain_red_path
 
     ###############################################################################
     #                  Set burnup steps
