cd8883e676311d26a7a52adbf0283fa15bcabe5e
Kalin Kiesling <krkiesling@gmail.com>
2023-04-10 09:24:30 -0500

    run logs for hybrid restart runs

--- deplete/results/pin/comp-redo/run_hy2.sh
+++ deplete/results/pin/comp-redo/run_hy2.sh
@@ -10,9 +10,9 @@ function run_hybrid () {
             mkdir -p ${cur_dir}/hybrid2/${n}/${g}
             cd ${cur_dir}/hybrid2/${n}/${g}
             ln -s ${main_dir}/model/chain_endfb71_pwr.xml .
-            python ${main_dir}/deplete/reduce_chain.py -c ${main_dir}/model/chain_endfb71_pwr.xml
+            python ${main_dir}/deplete/reduce_chain.py -c chain_endfb71_pwr.xml
             echo "Running Hybrid 2 ${g} ${n}"
-            mpiexec -n 20 python ${main_dir}/deplete/run_depletion.py -y 2 -n ${n} -g ${g} -m ${main_dir}/model/pin/ -c ${main_dir}/model/chain_endfb71_pwr_reduced.xml
+            mpiexec -n 20 python ${main_dir}/deplete/run_depletion.py -y 2 -n ${n} -g ${g} -m ${main_dir}/model/pin/ -c chain_endfb71_pwr_reduced.xml
         done
     done
 }
