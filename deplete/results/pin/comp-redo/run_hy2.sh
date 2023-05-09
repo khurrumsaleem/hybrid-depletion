4d680406ea0fe93ade2f41b8440b139dc7fb23a3
Kalin Kiesling <krkiesling@gmail.com>
2023-05-09 15:31:24 -0500

    modify run hy2 and redo

--- deplete/results/pin/comp-redo/run_hy2.sh
+++ deplete/results/pin/comp-redo/run_hy2.sh
@@ -1,18 +1,18 @@
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=48
 export cur_dir=`pwd`
 export main_dir="/home/kkiesling/depletion/hybrid-depletion"
 
 function run_hybrid () {
-    for g in 300 500 2500 10000
+    for g in 500 #300 500 2500 10000
     do
         for n in actinides mix
         do
-            mkdir -p ${cur_dir}/hybrid2/${n}/${g}
-            cd ${cur_dir}/hybrid2/${n}/${g}
+            mkdir -p ${cur_dir}/hybrid2-mod/${n}/${g}
+            cd ${cur_dir}/hybrid2-mod/${n}/${g}
             ln -s ${main_dir}/model/chain_endfb71_pwr.xml .
             python ${main_dir}/deplete/reduce_chain.py -c chain_endfb71_pwr.xml
             echo "Running Hybrid 2 ${g} ${n}"
-            mpiexec -n 20 python ${main_dir}/deplete/run_depletion.py -y 2 -n ${n} -g ${g} -m ${main_dir}/model/pin/ -c chain_endfb71_pwr_reduced.xml
+            mpiexec --bind-to socket -n 1 python ${main_dir}/deplete/run_depletion.py -y 2 -n ${n} -g ${g} -m ${main_dir}/model/pin/ -c chain_endfb71_pwr_reduced.xml
         done
     done
 }
