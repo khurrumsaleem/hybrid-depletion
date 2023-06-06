2cf70230079042ab611a3563d2c5a3f412a393e4
Kalin Kiesling <krkiesling@gmail.com>
2023-06-06 09:50:10 -0500

    modified hybrid 2 depletion runs

--- deplete/results/pin/comp-redo/run_hy2.sh
+++ deplete/results/pin/comp-redo/run_hy2.sh
@@ -9,7 +9,7 @@ function run_hybrid () {
         do
             mkdir -p ${cur_dir}/hybrid2-mod/${n}/${g}
             cd ${cur_dir}/hybrid2-mod/${n}/${g}
-            ln -s ${main_dir}/model/chain_endfb71_pwr.xml .
+            ln -sf ${main_dir}/model/chain_endfb71_pwr.xml .
             python ${main_dir}/deplete/reduce_chain.py -c chain_endfb71_pwr.xml
             echo "Running Hybrid 2 ${g} ${n}"
             mpiexec --bind-to socket -n 1 python ${main_dir}/deplete/run_depletion.py -y 2 -n ${n} -g ${g} -m ${main_dir}/model/pin/ -c chain_endfb71_pwr_reduced.xml
